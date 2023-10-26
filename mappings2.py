f6d82f397ddbfdf93d0bdefc8febca460bf2a4f2
Hanwen Zhang <70364944+homework36@users.noreply.github.com>
2023-10-26 10:18:27 -0400

    Add files via upload

---
+++ mappings2.py
@@ -0,0 +1,60 @@
+example_mapping = {
+  "mappings": {
+    "properties": {
+      "@id": {"type": "text"},
+      "@type": {"type": "text"},
+      "Dataset": {"properties": {"@id": {"type": "text"}}},
+      "P135": {
+        "properties": {
+          "@id": {"type": "text"},
+          "P2561": {"properties": {"@value": {"type": "text"}}}
+        }
+      },
+      "movement": {"type": "alias", "path": "P135.P2561.@value"},
+      "P136": {
+        "properties": {
+          "@id": {"type": "text"},
+          "@value": {"type": "text"},
+          "P2561": {"properties": {"@value": {"type": "text"}}}
+        }
+      },
+      "genre": {"type": "alias", "path": "P136.P2561.@value"},
+      "P1476": {"properties": {"@value": {"type": "text"}}},
+      "title": {"type": "alias", "path": "P1476.@value"},
+      "P1922": {"properties": {"@value": {"type": "text"}}},
+      "first line": {"type": "alias", "path": "P1922.@value"},
+      "P86": {
+        "properties": {
+          "@id": {"type": "text"},
+          "P2561": {"properties": {"@value": {"type": "text"}}}
+        }
+      },
+      "composer": {"type": "alias", "path": "P86.P2561.@value"},
+      "Q4484726": {"properties": {"@value": {"type": "text"}}},
+      "final": {"type": "alias", "path": "Q4484726.@value"},
+      "Q731978": {
+        "properties": {
+          "@id": {"type": "text"},
+          "@value": {"type": "text"},
+          "P2561": {"properties": {"@value": {"type": "text"}}}
+        }
+      },
+      "mode": {"type": "alias", "path": "Q731978.P2561.@value"},
+      "files": {
+        "properties": {
+          "@id": {"type": "text"},
+          "@type": {"type": "text"},
+          "P2701": {
+            "properties": {
+              "@id": {"type": "text"},
+              "P2561": {"properties": {"@value": {"type": "text"}}}
+            }
+          },
+          "file format": {"type": "alias", "path": "files.P2701.P2561.@value"}
+        }
+      },
+      "id-numbers": {"properties": {"@id": {"type": "text"}}},
+      "source": {"properties": {"@id": {"type": "text"}}}
+    }
+  }
+}
